9039b08cb495889108162dc4125c6d6025cf937b
Christian Soares Santos <christiansoares2009@gmail.com>
2024-01-20 16:04:26 -0300

    Update gateways

--- Domain/Interface/Gateways/IAcompanhamentoGateways.cs
+++ Domain/Interface/Gateways/IAcompanhamentoGateways.cs
@@ -1,12 +1,15 @@
 ﻿using Domain.Entities;
 
 namespace Domain.Interface.Gateways
-
-public interface IAcompanhamentoRepository
 {
-    public Task<IEnumerable<Acompanhamento>> GetAll();
-    public Task<Acompanhamento> GetById(int id);
-    public Task<int> Create(Acompanhamento acompanhamento);
-    public Task<bool> Update(Acompanhamento acompanhamento);
-    public Task<bool> Delete(int id);
+    public interface IAcompanhamentoRepository
+    {
+        public Task<IEnumerable<Acompanhamento>> GetAll();
+        public Task<Acompanhamento> GetById(int id);
+        public Task<int> Create(Acompanhamento acompanhamento);
+        public Task<bool> Update(Acompanhamento acompanhamento);
+        public Task<bool> Delete(int id);
+    }
 }
+
+
